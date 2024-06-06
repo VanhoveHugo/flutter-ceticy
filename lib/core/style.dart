ca319a7e8a77510e7eef9a0c7c86adec7e9693ff
VanhoveHugo <vanhovehugo1@gmail.com>
2024-06-06 22:31:41 +0200

    Fix Package Name

--- lib/core/style.dart
+++ lib/core/style.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter-ceticy/core/theme.dart';
+import 'package:ceticy/core/theme.dart';
 
 const appBar = TextStyle(
   color: ThemeColor.primary,
