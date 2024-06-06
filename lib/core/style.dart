f13792fe40be386c3b3d157cb8f528efd2731935
VanhoveHugo <vanhovehugo1@gmail.com>
2024-06-06 22:34:07 +0200

    Fix Style

--- lib/core/style.dart
+++ lib/core/style.dart
@@ -1,8 +1,2 @@
 import 'package:flutter/material.dart';
 import 'package:ceticy/core/theme.dart';
-
-const appBar = TextStyle(
-  color: ThemeColor.primary,
-  fontSize: 20,
-  fontWeight: FontWeight.bold
-);
