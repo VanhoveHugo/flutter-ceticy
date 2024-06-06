824d12c2fc6ce7d4d3ca20b57ac98b4fccad206c
VanhoveHugo <vanhovehugo1@gmail.com>
2024-06-06 22:28:55 +0200

    Rename App && Add Architecture

---
+++ lib/core/style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter-ceticy/core/theme.dart';
+
+const appBar = TextStyle(
+  color: ThemeColor.primary,
+  fontSize: 20,
+  fontWeight: FontWeight.bold
+);
