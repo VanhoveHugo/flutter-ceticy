8cf8978a0434175146fe595ed3a2eac71c5aff90
VanhoveHugo <vanhovehugo1@gmail.com>
2024-06-14 18:17:35 +0200

    Init MVP

--- lib/core/theme.dart
+++ lib/core/theme.dart
@@ -3,31 +3,44 @@ import 'package:flutter/material.dart';
 class AppColor {
   AppColor._();
 
-  static const Color primary50 = Color(0xFFFAF5FF);
-  static const Color primary100 = Color(0xFFF3E8FF);
-  static const Color primary200 = Color(0xFFEAD4FF);
-  static const Color primary300 = Color(0xFFD9B3FF);
-  static const Color primary400 = Color(0xFFC282FE);
-  static const Color primary500 = Color(0xFFAB53F9);
-  static const Color primary600 = Color(0xFF9731EC);
-  static const Color primary700 = Color(0xFF8120D0);
-  static const Color primary800 = Color(0xFF6E1FAA);
-  static const Color primary900 = Color(0xFF5A1B88);
-  static const Color primary950 = Color(0xFF3D0665);
+  static const Color primary50 = Color(0xFFFFFAEC);
+  static const Color primary100 = Color(0xFFFFF4D3);
+  static const Color primary200 = Color(0xFFFFE6A5);
+  static const Color primary300 = Color(0xFFFFD26D);
+  static const Color primary400 = Color(0xFFFFB332);
+  static const Color primary500 = Color(0xFFFF9A0A);
+  static const Color primary600 = Color(0xFFFF8200);
+  static const Color primary700 = Color(0xFFCC5F02);
+  static const Color primary800 = Color(0xFFA1490B);
+  static const Color primary900 = Color(0xFF823E0C);
+  static const Color primary950 = Color(0xFF461D04);
+
+  static const Color grey50 = Color(0xFFFAFAFA);
+  static const Color grey100 = Color(0xFFF4F4F5);
+  static const Color grey200 = Color(0xFFE4E4E7);
+  static const Color grey300 = Color(0xFFD4D4D8);
+  static const Color grey400 = Color(0xFFA1A1AA);
+  static const Color grey500 = Color(0xFF71717A);
+  static const Color grey600 = Color(0xFF52525B);
+  static const Color grey700 = Color(0xFF3F3F46);
+  static const Color grey800 = Color(0xFF27272A);
+  static const Color grey900 = Color(0xFF18181B);
 }
 
 class AppTheme {
   AppTheme._();
 
   static ThemeData theme = ThemeData(
+    scaffoldBackgroundColor: Colors.black,
     appBarTheme: const AppBarTheme(
+      elevation: 0,
+      backgroundColor: Colors.transparent,
       titleTextStyle: TextStyle(
         color: AppColor.primary400,
         fontSize: 20,
         fontWeight: FontWeight.bold,
       ),
     ),
-
     colorScheme: const ColorScheme(
       primary: AppColor.primary400,
       secondary: AppColor.primary600,
@@ -41,5 +54,17 @@ class AppTheme {
       onError: Colors.white,
       brightness: Brightness.light,
     ),
-  );  
+    bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      type: BottomNavigationBarType.fixed,
+      backgroundColor: Colors.black,
+      selectedItemColor: AppColor.primary400,
+      unselectedItemColor: Colors.white,
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color>(AppColor.primary400),
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+      ),
+    ),
+  );
 }
--- lib/src/views/homepage.dart
+++ lib/src/views/homepage.dart
@@ -1,48 +1,160 @@
 import 'package:flutter/material.dart';
+import 'package:ceticy/src/views/widgets/swipe.dart';
+import 'package:ceticy/core/style.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        title: Row(
+          children: [
+            // image png from assets
+            Image.asset(
+              'assets/images/logo.png',
+              width: 100,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            const Spacer(),
+            IconButton(
+              icon: SvgPicture.asset(
+                'assets/icons/Notification.svg',
+                width: 40,
+                height: 40,
+                color: Colors.white,
+              ),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: SvgPicture.asset(
+                'assets/icons/Settings.svg',
+                width: 40,
+                height: 40,
+                color: Colors.white,
+              ),
+              onPressed: () {},
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: Column(
+        children: [
+          const SwipeScreen(),
+          Container(
+            padding: const EdgeInsets.fromLTRB(24, 10, 24, 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: 56,
+                  width: 56,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                    color: Colors.white,
+                  ),
+                  child: IconButton(
+                    icon: const Icon(Icons.close),
+                    onPressed: () {},
+                  ),
+                ),
+                Row(
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.info_outline, color: Colors.white),
+                      onPressed: () {},
+                    ),
+                    const Text(
+                      "Info",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+                Container(
+                  height: 56,
+                  width: 56,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(16)),
+                    color: ThemeColor.primary,
+                  ),
+                  child: IconButton(
+                    icon: SvgPicture.asset(
+                      'assets/icons/Heart.svg',
+                      width: 30,
+                      height: 30,
+                      color: Colors.black,
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        color: Theme.of(context).colorScheme.primary,
+        child: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/Cards.svg',
+                width: 30,
+                height: 30,
+                color: ThemeColor.primary,
+              ),
+              label: 'Découvrir',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/Grid.svg',
+                width: 30,
+                height: 30,
+                color: Colors.white,
+              ),
+              label: 'Parcourir',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/Heart.svg',
+                width: 30,
+                height: 30,
+                color: Colors.white,
+              ),
+              label: 'Favoris',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/Messages.svg',
+                width: 30,
+                height: 30,
+                color: Colors.white,
+              ),
+              label: 'Messages',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/User.svg',
+                width: 30,
+                height: 30,
+                color: Colors.white,
+              ),
+              label: 'Profil',
+            ),
+          ],
+          currentIndex: 0,
+          selectedItemColor: Theme.of(context).colorScheme.onPrimary,
+          unselectedItemColor: Theme.of(context).colorScheme.onPrimary,
+        ),
       ),
     );
   }
